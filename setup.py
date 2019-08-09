34f5623085ae8536e02ec36b3d90a021eca60122
Zhuchang Zhan <zzhan@mit.edu>
2019-08-09 17:28:30 -0400

    updated code architecture and design decision

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 - Massachusetts Institute of Technology (MIT)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Reserved for future uses. 
+Will be needed potentially to check that all the packages required for simulation is installed.
+
+Code usable as is
+
+Packages needed for SEAS:
+
+
+numpy
+scipy
+matplotlib
+pandas
+
+tqdm
+
+
+"""
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
