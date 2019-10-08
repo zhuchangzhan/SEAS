9ee38d7cb352c3d64539729d0685f78d1e119df8
zhuchangzhan <azariven@gmail.com>
2019-10-08 12:01:23 -0400

    removed hardcode from simulated observation

--- example/Atmosphere_Simulation_Template/Example_Forward_Model.py
+++ example/Atmosphere_Simulation_Template/Example_Forward_Model.py
@@ -46,103 +46,69 @@ def Generate_Atmosphere_Spectra(user_input):
 
 @opt.timeit
 def Simulate_Atmosphere_Observation(user_input):
+       
+    nu_               = user_input["Spectra"]["Wavelength"]
+    Atmosphere_Height = user_input["Spectra"]["Atmosphere_Height"]
     
-    def sims(user_input):
-        nu_            = user_input["Spectra"]["Wavelength"]
-        Atmosphere_Height = user_input["Spectra"]["Atmosphere_Height"]
-        
-        noise = Photon_Noise(user_input)
-        bin_edges, bin_width, bin_centers = noise.determine_bin()
-        nu,  convolved_Height = noise.calculate_convolve(nu_, Atmosphere_Height)    
-        
-        D_atmosphere, bin_edges, binnumber = stats.binned_statistic(10000./nu_[::-1], Atmosphere_Height[::-1], bins=bin_edges)
-    
-        user_input["Spectra"]["bin_width"]    = bin_width
-        user_input["Spectra"]["bin_centers"]  = bin_centers
-        user_input["Spectra"]["nu"]           = nu
-        user_input["Spectra"]["bin_values"]   = convolved_Height
-        user_input["Spectra"]["D_atmosphere"] = D_atmosphere
-        
-    
-        return user_input,nu,convolved_Height,bin_centers,bin_width,D_atmosphere
+    noise = Photon_Noise(user_input)
+    bin_edges, bin_width, bin_centers = noise.determine_bin()
+    nu,  convolved_Height = noise.calculate_convolve(nu_, Atmosphere_Height)    
+    D_atmosphere, bin_edges, binnumber = stats.binned_statistic(10000./nu_[::-1], Atmosphere_Height[::-1], bins=bin_edges)
 
-    def simulated_observation(molecule,stellar,nu,bio_trans,bin_centers,bin_width,D_atmosphere,observe_hour):
+    user_input["Spectra"]["bin_width"]    = bin_width
+    user_input["Spectra"]["bin_centers"]  = bin_centers
+    user_input["Spectra"]["nu"]           = nu
+    user_input["Spectra"]["bin_values"]   = convolved_Height
+    user_input["Spectra"]["D_atmosphere"] = D_atmosphere
     
-        if stellar == "Sun":
-            T_Star = 5770.
-            R_Star = R_Sun
+    main_mol    = user_input["Prototype"]["Atmosphere_Type"]
+    stellar     = user_input["Prototype"]["Stellar_Type"]
+    R_Planet    = float(user_input["Planet"]["R_Planet"])*R_Earth
+    R_Star      = float(user_input["Star"]["R_Star"])*R_Sun
+    T_Star      = float(user_input["Star"]["T_Star"])
+    Distance    = float(user_input["System"]["D_Star_Observer"])*Psec  
+    
+    R_obs       = float(user_input["Telescope"]["Aperture"])
+    Duration    = float(user_input["Telescope"]["Duration"])*3600. # hr to second
+    Quantum     = float(user_input["Telescope"]["Quantum_Efficiency"])
+    Noise_M     = float(user_input["Telescope"]["Noise"]["multiplier"])
+    
+    # calculate number of photons 
+    B_Body      = blackbody_lam(bin_centers*10**-6, T_Star)
+    Bin_width   = bin_width*10**-6
+    A_Star      = np.pi*R_Star**2
+    Psi_Tele    = np.pi*R_obs**2/Distance**2
+    E_Total     = B_Body*Bin_width*A_Star*Psi_Tele*Duration
+    star_photon = (E_Total*bin_centers*10**-6)/(HPlanck*CLight)*Quantum
+    
+    signal = R_Planet**2/R_Star**2*star_photon
+    photon_noise = Noise_M*np.sqrt(star_photon)
+    
+    SNR = signal/photon_noise
+    
+    a_sig,b_sig,c_sig = [],[],[]
+    a_noise,b_noise,c_noise = [],[],[]
+    
+    for i,sig,noise in zip(bin_centers,signal,photon_noise):
+        if i> 1 and i < 5:
+            a_sig.append(sig)
+            a_noise.append(noise)
+        if i >= 5 and i < 12:
+            b_sig.append(sig)
+            b_noise.append(noise)
+        if i >= 12:
+            c_sig.append(sig)
+            c_noise.append(noise)
             
-        elif stellar == "Ma":
-            T_Star = 3000.
-            R_Star = 0.26*R_Sun
-        
-        elif stellar == "Mq":
-            T_Star = 3000.
-            R_Star = 0.26*R_Sun
-                
-        #bio_molecule = "PH3"
-        #molecule = "CO2"
-        #stellar = "Sun"
-        g_atm = 3200. 
-        if molecule == "H2":
-            mu_atm = 4.6*amu2g
-        elif molecule == "N2":
-            mu_atm = 28*amu2g
-        elif molecule == "CO2":
-            mu_atm = 44*amu2g   
+    SNR_Broad = [np.sum(a_sig)/np.sqrt(np.sum(np.array(a_noise)**2)),
+               np.sum(b_sig)/np.sqrt(np.sum(np.array(b_noise)**2)),
+               np.sum(c_sig)/np.sqrt(np.sum(np.array(c_noise)**2))]
     
-        R_obs        = 6.5
-        R_Planet     = 6400000*1.75
-        Distance     = 10*Psec  # 10 pc to meter
-        Duration     = observe_hour*3600. # 10 hr to second
-        Quantum      = 0.3 
-        Noise_M      = 1.5
-        
-        # calculate number of photons 
-        B_Body = blackbody_lam(bin_centers*10**-6, T_Star)
-        Bin_width = bin_width*10**-6
-        A_Star = np.pi*R_Star**2
-        Psi_Tele = np.pi*R_obs**2/Distance**2
-        E_Total = B_Body*Bin_width*A_Star*Psi_Tele*Duration
-        star_photon = (E_Total*bin_centers*10**-6)/(HPlanck*CLight)*Quantum
-        
-        #signal = (2*R_planet*D_atmosphere)/R_Star**2*num_photon
-        
-        R_Planet = R_Planet+D_atmosphere
-        
-        signal = R_Planet**2/R_Star**2*star_photon
-        photon_noise = Noise_M*np.sqrt(star_photon)
-        
-        
-        SNR = signal/photon_noise
-        
-        a_sig,b_sig,c_sig = [],[],[]
-        a_noise,b_noise,c_noise = [],[],[]
-        for i,sig,noise in zip(bin_centers,signal,photon_noise):
-            if i> 1 and i < 5:
-                a_sig.append(sig)
-                a_noise.append(noise)
-            if i >= 5 and i < 12:
-                b_sig.append(sig)
-                b_noise.append(noise)
-            if i >= 12:
-                c_sig.append(sig)
-                c_noise.append(noise)
-        SNR_ATM = [np.sum(a_sig)/np.sqrt(np.sum(np.array(a_noise)**2)),
-                   np.sum(b_sig)/np.sqrt(np.sum(np.array(b_noise)**2)),
-                   np.sum(c_sig)/np.sqrt(np.sum(np.array(c_noise)**2))]
-        
-        #SNR_ATM = np.sum(signal)/np.sqrt(np.sum(photon_noise**2))
-        
-        
-        depth = R_Planet**2/R_Star**2#*1e6
-        
-        
-        #bin_means_bio,SNR_bio = simulate_observation(noise,bin_edges,nu_,Bio_Transit_Signal)
-        
-        return R_Planet,depth,SNR,SNR_ATM
-        
-    def cal_SNR(bio_depth,bio_transit_depth_bin,b_SNR_bio,b_ATM_SNR_bio):
+    ref_depth = (convolved_Height+R_Planet)**2/R_Star**2
+    ref_depth_bin = (D_atmosphere+R_Planet)**2/R_Star**2
+
+
+    def cal_binned_SNR(bio_depth,bio_transit_depth_bin,b_SNR_bio,b_ATM_SNR_bio):
 
         #plt.plot(10000./nu,(bio_depth-ref_depth)*1e6)
         #plt.plot(b_wav,b_SNR_bio)
@@ -197,26 +163,13 @@ def Simulate_Atmosphere_Observation(user_input):
         
         return Max_SNR,mean_atmosphere_radius_per_wavelength,standard_error_atmosphere_radius_per_wavelength,mean_atmosphere_radius_,standard_error_atmosphere_radius_,atm_diff,atm_error
 
-    user_input,nu,ref_trans,b_wav,b_width,b_D_atm_ref = sims(user_input)
-    
-    main_mol = "CO2"
-    stellar = "Ma"
-    observe_hour = 100
-    R_Planet = 6400000
-    R_Star = 0.26*R_Sun
-    
-    b_R_planet_ref,b_depth_ref,b_SNR_ref,b_ATM_SNR_ref = simulated_observation(main_mol,stellar,nu,ref_trans,b_wav,b_width,b_D_atm_ref,observe_hour)
+    result = cal_binned_SNR(ref_depth,ref_depth_bin,SNR,SNR_Broad)
+    ATM_Max_SNR_ref,a,b,c,d,atm_diff_ref,atm_error_ref = result
 
-    ref_depth = (ref_trans+R_Planet)**2/R_Star**2
-    ref_depth_bin = (b_D_atm_ref+R_Planet)**2/R_Star**2
-
-    result = cal_SNR(ref_depth,ref_depth_bin,b_SNR_ref,b_ATM_SNR_ref)
-    ATM_Max_SNR_ref,aaa,bbb,ccc,ddd,atm_diff_ref,atm_error_ref = result
-
-    mean_atmosphere_radius_per_wavelength_ref = aaa
-    standard_error_atmosphere_radius_per_wavelength_ref = bbb
-    mean_atmosphere_radius_ref_ = ccc
-    standard_error_atmosphere_radius_ref_ = ddd 
+    mean_atmosphere_radius_per_wavelength_ref = a
+    standard_error_atmosphere_radius_per_wavelength_ref = b
+    mean_atmosphere_radius_ref_ = c
+    standard_error_atmosphere_radius_ref_ = d 
 
     mean_atmosphere_wav     = np.array([3,8.5,18.5])
     mean_atmosphere_width   = np.array([2,3.5,6.5])
@@ -225,9 +178,7 @@ def Simulate_Atmosphere_Observation(user_input):
     ax1 = fig.add_subplot(111) 
     ax1.plot(10000./nu,ref_depth*1e6,label="w/o PH$_3$")
 
-
-
-    ax1.errorbar(b_wav,
+    ax1.errorbar(bin_centers,
                  (mean_atmosphere_radius_per_wavelength_ref)*1e6,
                  yerr=standard_error_atmosphere_radius_per_wavelength_ref*1e6,
                  capsize=4, markersize=6,fmt='--o',label="w/o PH$_3$ Binned",color="0.5")
